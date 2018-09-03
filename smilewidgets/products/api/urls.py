14a683e53794d42a5d418ed4213694fbfa5bc4a7
Vlad Turlai <turlaivlad@gmail.com>
2018-09-03 15:57:11 +0300

    Create a ProductPrice APIView

--- smilewidgets/products/api/urls.py
+++ smilewidgets/products/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
+from products.api.views import ProductPriceAPIView
 
 urlpatterns = [
-
+    path('get-price/', ProductPriceAPIView.as_view())
 ]
---
+++ smilewidgets/products/api/views.py
@@ -0,0 +1,6 @@
+from rest_framework.views import APIView
+
+
+class ProductPriceAPIView(APIView):
+    def get(self, request):
+        pass
